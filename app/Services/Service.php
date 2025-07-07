d9bd0b64ba3e5599116d28c7d384f6302925575d
smme2009 <smme2009@gmail.com>
2025-07-07 17:53:33 +0800

    feat: 新增Service的通用驗證器

--- app/Services/Service.php
+++ app/Services/Service.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Services\Tool\Result as SrcToolResult;
+use App\Services\Tool\Validator as SrcToolValidator;
 
 /**
  * Service通用層，可以用來存放共用的工具和方法
@@ -17,5 +18,18 @@ class Service
     public function toolResult(): SrcToolResult
     {
         return new SrcToolResult();
+    }
+
+    /**
+     * 取得Service的通用驗證器
+     *
+     * @param array $data 驗證資料
+     * @param array $rule 規則
+     * 
+     * @return SrcToolValidator
+     */
+    public function toolValidator(array $data, array $rule): SrcToolValidator
+    {
+        return new SrcToolValidator($data, $rule);
     }
 }
