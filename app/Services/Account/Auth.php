51be4eb574f15a26dcfd059f9daea849a72ba48f
smme2009 <smme2009@gmail.com>
2025-11-12 20:47:23 +0800

    feat: 將Service中的回傳結果狀態調整為HTTP Code

--- app/Services/Account/Auth.php
+++ app/Services/Account/Auth.php
@@ -36,7 +36,7 @@ class Auth extends Service
         // 解碼失敗，回傳錯誤資料
         if (empty($data) === true) {
             return $this->toolResult()
-                ->setStatus(false)
+                ->setHttpCode(401)
                 ->setMessage('驗證失敗，Jwt Token無效')
                 ->build();
         }
@@ -47,14 +47,14 @@ class Auth extends Service
         // 驗證失敗，回傳錯誤資料
         if ($isInWhitelist === false) {
             return $this->toolResult()
-                ->setStatus(false)
+                ->setHttpCode(401)
                 ->setMessage('驗證失敗，Jwt Token無效')
                 ->build();
         }
 
         // 解碼成功，回傳資料
         return $this->toolResult()
-            ->setStatus(true)
+            ->setHttpCode(200)
             ->setMessage('驗證成功')
             ->bulkAddData($data)
             ->build();
--- app/Services/Account/Info.php
+++ app/Services/Account/Info.php
@@ -36,14 +36,14 @@ class Info extends Service
         // 查詢帳號失敗，回傳錯誤資料
         if ($model === null) {
             return $this->toolResult()
-                ->setStatus(false)
+                ->setHttpCode(404)
                 ->setMessage('取得帳號資料失敗，查無此帳號')
                 ->build();
         }
 
         // 取得帳號資料成功，回傳帳號資料
         return $this->toolResult()
-            ->setStatus(true)
+            ->setHttpCode(200)
             ->setMessage('取得帳號資料成功')
             ->addData('account', $model->account)
             ->addData('name', $model->name)
