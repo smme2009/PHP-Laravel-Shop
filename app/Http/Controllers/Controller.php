a6ed0e47e39665eef1e07e4a79f5007f36d60664
smme2009 <smme2009@gmail.com>
2025-11-12 21:45:41 +0800

    chore: 移除棄用的Controller通用回應物件建構工具

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,21 +7,10 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\JsonResponse;
 use App\Services\Tool\Output\Result as SvcResult;
-use App\Http\Controllers\Tool\Response as ToolResponse;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    /**
-     * 取得Controller的通用回應物件建構工具
-     *
-     * @return ToolResponse
-     */
-    protected function toolResponse(): ToolResponse
-    {
-        return new ToolResponse();
-    }
 
     /**
      * 取得JSON回應
