cc5dba4b12366bc414abaf6a275e452352d65ef6
smme2009 <smme2009@gmail.com>
2025-11-12 21:41:18 +0800

    feat: 新增Controller通用的取得JSON回應函式

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+use Illuminate\Http\JsonResponse;
+use App\Services\Tool\Output\Result as SvcResult;
 use App\Http\Controllers\Tool\Response as ToolResponse;
 
 class Controller extends BaseController
@@ -20,5 +21,22 @@ class Controller extends BaseController
     protected function toolResponse(): ToolResponse
     {
         return new ToolResponse();
+    }
+
+    /**
+     * 取得JSON回應
+     * 
+     * @param SvcResult $result
+     * 
+     * @return JsonResponse JSON回應
+     */
+    protected function getJsonResponse(SvcResult $result): JsonResponse
+    {
+        $responseData = [
+            'message' => $result->message,
+            'data' => $result->data,
+        ];
+
+        return response()->json($responseData, $result->httpCode);
     }
 }
