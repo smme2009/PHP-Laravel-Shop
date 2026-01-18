a4e3ad23fd974faedcf410f8ee0ee6808626840c
smme2009 <smme2009@gmail.com>
2026-01-18 14:12:00 +0800

    feat: 加入設定CORS參數的機制

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
 
     'allowed_origins_patterns' => [],
 
@@ -27,7 +27,7 @@ return [
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => env('CORS_MAX_AGE', 0),
 
     'supports_credentials' => false,
 
