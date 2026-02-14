d21ac550426e917548adee1d5fed2d41e5234d2b
smme2009 <smme2009@gmail.com>
2026-02-14 23:20:44 +0800

    feat: 增加發送API紀錄統計通知的排程

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // 發送API紀錄統計通知(6小時一次)
+        $schedule->command('app:send-grouped-api-log')->everySixHours();
     }
 
     /**
@@ -20,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
