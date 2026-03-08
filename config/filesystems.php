f6ff60383e8f16d56935522738ccee3230d3dc4c
smme2009 <smme2009@gmail.com>
2026-03-08 17:23:44 +0800

    chore: 設定Supabase的S3參數

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,18 @@ return [
             'throw' => false,
         ],
 
+        'supabase' => [
+            'driver' => 's3',
+            'key' => env('SUPABASE_ACCESS_KEY_ID'),
+            'secret' => env('SUPABASE_SECRET_ACCESS_KEY'),
+            'region' => env('SUPABASE_DEFAULT_REGION'),
+            'bucket' => env('SUPABASE_BUCKET'),
+            'url' => env('SUPABASE_URL'),
+            'endpoint' => env('SUPABASE_ENDPOINT'),
+            'use_path_style_endpoint' => env('SUPABASE_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
         'test' => [
             'driver' => 'local',
             'root' => storage_path('app/test'),
